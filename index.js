97c6d67b79460cf2e5921665accb8ce0bf8aa893
ahasana-ncs13 <ahasanancs0485@gmail.com>
2025-12-14 12:43:31 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,36 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const express = require('express')
+const app = express()
+require('dotenv').config()
+const port =process.env.PORT || 3000
+
+const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASSWORD}@cluster0.j6dmigp.mongodb.net/?appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    await client.connect();
+
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+  }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+  res.send('Public Infrastructure Issue Reporting System server is running!')
+})
+app.listen(port, () => {
+  console.log(`Public Infrastructure Issue Reporting System app listening on port ${port}`)
+})
